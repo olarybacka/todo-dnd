2eafcff1608dce36f0bf1ca16b7dde93b66afe52
Ola Rybacka <ola.rybacka@platform24.se>
2021-11-07 16:16:39 +0100

    add dragging events

--- src/components/Columns/ColumnContainer.tsx
+++ src/components/Columns/ColumnContainer.tsx
@@ -1,3 +1,4 @@
+import { DragEvent } from 'react'
 import { TaskContainer } from 'components/TaskContainer'
 import { FC } from 'react'
 import { Column } from './column'
@@ -9,11 +10,19 @@ type Props = {
   column: Column
 }
 
+const handleDragOver = (e: DragEvent) => {
+  e.preventDefault()
+}
+
 export const ColumnContainer: FC<Props> = ({ column }) => {
   const { tasks } = useTaskListContext()
 
+  const handleOnDrop = (e: DragEvent) => {
+    console.log(e.dataTransfer.getData('id'))
+  }
+
   return (
-    <S.Section>
+    <S.Section onDrop={handleOnDrop} onDragOver={handleDragOver}>
       <h2>{column.name}</h2>
       <S.TasksListContainer>
         {tasks
--- src/components/TaskContainer/TaskContainer.tsx
+++ src/components/TaskContainer/TaskContainer.tsx
@@ -1,3 +1,4 @@
+import { DragEvent } from 'react'
 import * as S from './TaskContainer.styled'
 import { Task } from './task';
 import { FC } from 'react';
@@ -6,9 +7,13 @@ type Props = {
   task: Task
 }
 
+const handleDragStart = (e: DragEvent, id: string) => {
+  e.dataTransfer.setData('id', id)
+}
+
 export const TaskContainer: FC<Props> = ({task}) => {
   return (
-    <S.Task draggable>
+    <S.Task draggable onDragStart={e => handleDragStart(e, task.id)}>
       {task.name} 
     </S.Task>
   )
